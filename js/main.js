d528d3bda708d7c0bc21b0ce9a5e26e6b0caa78e
Nick Collings <nick@collingo.com>
2012-08-30 00:41:31 +0100

    delegated drag events

--- js/main.js
+++ js/main.js
@@ -13,19 +13,26 @@ $(function() {
 	}
 
 	window.down = {};
+	var beingDragged;
 
-	$("li").bind('mousedown', function(e) {
-		// console.log(e, e.target,  e.pageX - e.target.offsetTop, e.pageY - e.target.offsetLeft);
-		console.log(e.pageX, "-", e.target.offsetTop, e.pageY - e.target.offsetLeft);
+	$("ul").bind('mousedown', function(e) {
 		down.x = e.pageX - e.target.offsetLeft;
 		down.y = e.pageY - e.target.offsetTop;
+		beingDragged = $(e.target);
+		beingDragged.addClass("dragged");
 	}).hammer({prevent_default:true}).bind("drag", function(e) {
-		throttle(onDrag, window, e);
+		//throttle(onDrag, window, e);
+		onDrag(e);
+	}).bind('mouseup', function(e) {
+		$("li").removeClass("dragged");
 	});
 
 	function onDrag(e) {
-		console.log(Date.now(), "drag", e);
-		$(e.target).css({top:e.touches[0].y - down.y, left:e.touches[0].x - down.x});
+
+        var touches = e.originalEvent.touches || [e.originalEvent];
+        for(var t=0; t<touches.length; t++) {
+			$(beingDragged).css({top:e.touches[0].y - down.y, left:e.touches[0].x - down.x});
+        }
 	}
 
 });

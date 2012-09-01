74478d0a6e0dab6a3a43bc4dfe61a64f9f53cea9
Nick Collings <nick@collingo.com>
2012-09-01 13:26:57 +0100

    fixed delayed bindings, working on mobile

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
 $(function() {
 
-	var safe = 1;
+	var safe = 1,
+		beingDragged,
+		holdTimer,
+		grab = {},
+		start = {},
+		hold = {x:0,y:0},
+		element = $("ul");
 
+	// utility
 	function throttle(method, scope, e) {
 		if(safe) {
 			safe = 0;
@@ -12,61 +19,79 @@ $(function() {
 		}
 	}
 
-	window.down = {};
-	window.start = {};
-	window.move = {x:0,y:0};
-	var beingDragged;
-	var holdTimer;
+	function touchHandler(event) {
+		var touches = event.changedTouches,
+			first = touches[0],
+			type = "";
 
-	$("ul").hammer({prevent_default:true}).bind('mousedown', function(e) {
+		switch(event.type) {
+			case "touchstart": type="mousedown"; break;
+			case "touchmove":  type="mousemove"; break;        
+			case "touchend":   type="mouseup"; break;
+			default: return;
+		}
+
+		var simulatedEvent = document.createEvent("MouseEvent");
+		simulatedEvent.initMouseEvent(type, true, true, window, 1, 
+								  first.screenX, first.screenY, 
+								  first.clientX, first.clientY, false, 
+								  false, false, false, 0/*left*/, null);
+		first.target.dispatchEvent(simulatedEvent);
+		event.preventDefault();
+	}
+	document.addEventListener("touchstart", touchHandler, true);
+	document.addEventListener("touchmove", touchHandler, true);
+	document.addEventListener("touchend", touchHandler, true);
+	document.addEventListener("touchcancel", touchHandler, true); 
+
+
+
+	element.hammer({prevent_default:true}).bind('mousedown', onStart).bind('mouseup', onEnd);
+
+	function onStart(e) {
 
 		if(e.target.tagName === "LI") {
 
-			start = {
-				x: e.pageX,
-				y: e.pageY
-			};
-			down = {
-				x: e.pageX - e.target.offsetLeft,
-				y: e.pageY - e.target.offsetTop
-			};
+			start.x = hold.x = e.pageX;
+			start.y = hold.y = e.pageY;
+			grab.x = e.pageX - e.target.offsetLeft;
+			grab.y = e.pageY - e.target.offsetTop;
 			beingDragged = $(e.target);
 
-			$("ul").bind('mousemove', onMouseMove);
+			element.bind('mousemove', onMoveDuringHold);
 
-			holdTimer = setTimeout(function() {
-				if((move.x === 0 && move.y === 0) || Math.sqrt(Math.pow(move.x - start.x, 2) + Math.pow(move.y - start.y, 2)) < 30) {
-					beingDragged.addClass("dragged");
-					$("ul").bind("drag", function(e) {
-						//throttle(onDrag, window, e);
-						onDrag(e);
-					});
-				} else {
-					$("ul").unbind('mousemove', onMouseMove);
-					move = {x:0,y:0};
-				}
-			}, 1000);
+			holdTimer = setTimeout(onHoldTimerComplete, 200);
 
 		}
 
-	}).bind('mouseup', function(e) {
-		clearTimeout(holdTimer);
-		$("ul").unbind('drag');
-		$("ul").unbind('mousemove', onMouseMove);
-		$("li").removeClass("dragged");
-	});
-
-	function onMouseMove(e) {
-		move.x = e.pageX;
-		move.y = e.pageY;
 	}
 
-	function onDrag(e) {
+	function onHoldTimerComplete() {
+		element.unbind('mousemove', onMoveDuringHold);
+		if(Math.sqrt(Math.pow(hold.x - start.x, 2) + Math.pow(hold.y - start.y, 2)) < 30) {
+			element.bind("drag", onMoveDuringDrag);
+			// element.bind('mousemove', onMouseMoveDuringDrag);
+			beingDragged.addClass("dragged");
+		}
+	}
 
-        var touches = e.originalEvent.touches || [e.originalEvent];
-        for(var t=0; t<touches.length; t++) {
-			$(beingDragged).css({top:e.touches[0].y - down.y, left:e.touches[0].x - down.x});
-        }
+	function onMoveDuringHold(e) {
+		hold.x = e.pageX;
+		hold.y = e.pageY;
+	}
+
+	function onMoveDuringDrag(e) {
+		var touches = e.originalEvent.touches || [e.originalEvent];
+		for(var t=0; t<touches.length; t++) {
+			$(beingDragged).css({top:e.touches[0].y - grab.y, left:e.touches[0].x - grab.x});
+		}
+	}
+
+	function onEnd(e) {
+		clearTimeout(holdTimer);
+		element.unbind('drag');
+		element.unbind('mousemove', onMoveDuringHold);
+		$("li").removeClass("dragged");
 	}
 
 });
